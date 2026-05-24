f62ccece014a9215546e703a317ab1165d38055c
Harley447 <arleyeduardomantillavelasquez@gmail.com>
2026-05-24 17:48:13 -0500

    se añade genero y informalidad o formalidad

--- Cambios_Cesar.R
+++ Cambios_Cesar.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(lmtest)
+library(GGally)
 # ── IMPORTAR ──────────────────────────────────────────────────────────────────
 fuerzat_mayo  <- read_delim("/home/arley/Estadistica_2/Datos/Mayo_2025/CSV/Fuerza de trabajo.CSV",
                             delim = ";", escape_double = FALSE, trim_ws = TRUE)
@@ -40,7 +41,12 @@ base_data <- ocupados_mayo %>%
         DIRECTORIO,
         ORDEN,
         P6040,   # Edad
-        P3042    # Nivel educativo
+        P3042,    # Nivel educativo
+        P3271,     #Sexo al nacer
+      ),
+    ocupados_mayo %>% 
+      select(
+        P6440      #Tiene contrato si o no
       ),
     by = c("DIRECTORIO", "ORDEN")
   )
@@ -49,17 +55,29 @@ base_data <- ocupados_mayo %>%
 base_data <- base_data %>%
   mutate(
     educacion_anios = case_when(
-      P3042 <= 5 ~ 5,
-      P3042 <= 8 ~ 11,
-      P3042 >= 9 ~ 16,
+      P3042 == 1  ~ 0,   # Ninguno
+      P3042 == 2  ~ 1,   # Preescolar
+      P3042 == 3  ~ 5,   # Básica primaria
+      P3042 == 4  ~ 9,   # Básica secundaria
+      P3042 == 5  ~ 11,  # Media académica
+      P3042 == 6  ~ 11,  # Media técnica
+      P3042 == 7  ~ 13,  # Normalista
+      P3042 == 8  ~ 13,  # Técnica profesional
+      P3042 == 9  ~ 15,  # Tecnológica
+      P3042 == 10 ~ 16,  # Universitaria
+      P3042 == 11 ~ 17,  # Especialización
+      P3042 == 12 ~ 18,  # Maestría
+      P3042 == 13 ~ 22,  # Doctorado
       TRUE ~ NA_real_
     )
   )
 
 base_data <- base_data %>%
   mutate(
-    experiencia =
-      P6040 - educacion_anios - 6
+    experiencia = pmax(
+      P6040 - educacion_anios - 6,
+      0
+    )
   )
 
 base_data <- base_data %>%
@@ -76,12 +94,43 @@ m_mincer <- lm(
   ingreso_log ~
     educacion_anios +
     experiencia +
-    experiencia2,
+    experiencia2 +
+    factor(P3271),
   data = base_data
 )
 
 summary(m_mincer)
 
 
+base_cor1 <- base_data %>% 
+  select(
+    educacion_anios,
+    ingreso_log,
+    experiencia,
+    experiencia2
+  ) %>%
+  na.omit()
+cor(base_cor1)
 
+#Grafica de correlación 
+base_data %>%
+  select(
+    educacion_anios,
+    ingreso_log,
+    experiencia,
+    experiencia2
+  ) %>%
+  ggpairs()
 
+base_formal <- base_data %>%
+  filter(P6440 == 1)
+m_formal <- lm(
+  ingreso_log ~
+    educacion_anios +
+    experiencia +
+    experiencia2 +
+    factor(P3271),
+  data = base_formal
+)
+
+summary(m_formal)
